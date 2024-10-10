d22d17d2691337a8729281e6af32045f3f63904e
bobbyhiddn <mlmicahlongmire@gmail.com>
2024-10-09 19:59:14 -0600

    initial

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
+// server.js
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Read the GitCommandSimulator component file
-const GitCommandSimulatorCode = fs.readFileSync(path.join(__dirname, 'GitCommandSimulator.js'), 'utf8');
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-  const html = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Git Command Simulator</title>
-        <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
-        <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-      </head>
-      <body>
-        <div id="root">If you see this, React hasn't rendered anything yet.</div>
-        <script>
-          ${GitCommandSimulatorCode}
-          ReactDOM.render(React.createElement(GitCommandSimulator), document.getElementById('root'));
-        </script>
-      </body>
-    </html>
-  `;
-  res.send(html);
+// Handle any unmatched routes by serving 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
